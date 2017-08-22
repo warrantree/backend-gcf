60a3be54f87ed045afa786d5be690dbd26c2eb84
Aditya Mishra <aditya.mishra@upscale.vc>
2017-08-22 16:10:23 +0530

    test-tutorial2-v00001

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,6 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello and welcome to ShopedDB!");
+ console.log('this is a log from fb functions');
 });
